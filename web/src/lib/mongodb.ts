4fbc56bc5261e169acc5e2c161e68e28064b120c
deeznuts1111 <phuc1haiba@gmail.com>
2025-12-24 01:13:28 +0700

    Create mongodb.ts

---
+++ web/src/lib/mongodb.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://phucdz:phuc9807@shooly.e65ipe6.mongodb.net/?appName=shooly";
+const MONGODB_DB = process.env.MONGODB_DB || "supershoply";
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      dbName: MONGODB_DB,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
